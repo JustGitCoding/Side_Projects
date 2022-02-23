6c536eeb2127c3912d5bf87a10856d6ee8e6a8c8
Justin K <justinhkiang@gmail.com>
2022-02-23 10:56:24 -0800

    added random key generator

--- Infinite_Time_Loop.py
+++ Infinite_Time_Loop.py
@@ -10,7 +10,19 @@ totpausetime = 0
 loopcount = 0
 tot_time = time.time()
 
-
+def generate_key(length):
+    letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    numbers=['0','1','2','3','4','5','6','7','8','9']
+    ln=['n','n','n','l','l']
+    key = []
+    for i in range(length):
+        pick=random.choice(ln)
+        if pick=='n':
+            key.append(random.choice(numbers))
+        elif pick == 'l':
+            key.append(random.choice(letters))
+    fullkey = "".join(key)
+    return fullkey
 
 # Infinite loop
 while runstart <= datetime.now():
@@ -31,11 +43,14 @@ while runstart <= datetime.now():
         loop_time = loop_end - loop_start
         tot_time = loop_end - runstart
 
+        # Function Key
+        instance_key = generate_key(8)
+
         # Print random output
         randomoutput = (
                 f">>cmd.prmpt/..st_time={loop_start} "
                 f"cdir ip/#!stamp c{loopcount}:{number*2:05d} // {totpausetime:.15f}\n"
-                f"  git.f~pull;time_end={loop_end}>./input.csv --p{int(number/10)}:{pausetime:.4f}/tot=<{tot_time}s>\n\n"
+                f"   [pw.{instance_key}] time={loop_end}>./input.csv --p{int(number/10)}:{pausetime:.4f}/tot=<{tot_time}s>\n\n"
                 )
         print(randomoutput,end="")
         with open(outputfile,"a") as txt_file:
