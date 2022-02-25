40f9ce5c2049eaf8080bf726f44ac2b6a9a2b579
Justin K <justinhkiang@gmail.com>
2022-02-25 12:40:57 -0800

    more hacker rank challenges

--- Hacker_Rank/Python_HR.py
+++ Hacker_Rank/Python_HR.py
@@ -59,3 +59,50 @@ n=5
 for i in range(1,n+1):
     print(i,end="")
 
+
+# LIST COMPREHENSION
+# You are given three integers x, y and z representing the dimensions of a cuboid along with an integer n. Print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to n. 
+# Here, 0<=i<=x; 0<=j<=y; 0<=k<=z. Please use list comprehensions rather than multiple loops, as a learning exercise.
+# Print the list in lexicographic increasing order.
+x=1
+y=1
+z=1
+n=2
+list = []
+print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n])
+
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+# The first line contains n scores. The second line contains an array A of n integers each separated by a space.
+# Print the runner-up score
+n=5
+arr1 = map(int, input().split())
+runnerup=-100
+arr=list(arr1)
+for i in range(n):
+    if (arr[i] > runnerup and arr[i] < max(arr)):
+        runnerup = arr[i]
+print(runnerup)
+
+# Given the names and grades for each student in a class of n students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+# Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+import numpy as np
+
+n=3
+data = []
+losers = []
+lowest = 100
+for i in range(n):
+    name = input()
+    score = float(input())
+    data.append([name,score])
+
+for i in range(n):
+    if data[i][1] <= lowest:
+        lowest = data[i][1]
+
+for i in range(n):
+    if data[i][1] == lowest:
+        losers.append([data[i][0],data[i][1]])
+
+print(sorted(losers))
