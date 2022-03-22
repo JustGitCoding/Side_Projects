ec9ad5e9e44889241b137bf04062404a0f1f26e6
Justin K <justinhkiang@gmail.com>
2022-03-21 17:13:56 -0700

    upgraded summarize function to show dealer 2nd card depending on optional argument. added option for players to make wagers. simplified code for tracking player hit activity

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,19 +1,18 @@
 # Dependencies
 import random
 
-# Variables
-player_count = 1
+# Card Variables
 ss = '\u2660'
 cc = '\u2663'
 hh = '\u2665'
 dd = '\u2666'
 unshuffled_deck = [f'A {dd}', f'2 {dd}', f'3 {dd}', f'4 {dd}', f'5 {dd}', f'6 {dd}', f'7 {dd}', f'8 {dd}', f'9 {dd}', f'10{dd}', f'J {dd}', f'Q {dd}', f'K {dd}',
-                        f'A {cc}', f'2 {cc}', f'3 {cc}', f'4 {cc}', f'5 {cc}', f'6 {cc}', f'7 {cc}', f'8 {cc}', f'9 {cc}', f'10{cc}', f'J {cc}', f'Q {cc}', f'K {cc}',
-                        f'A {hh}', f'2 {hh}', f'3 {hh}', f'4 {hh}', f'5 {hh}', f'6 {hh}', f'7 {hh}', f'8 {hh}', f'9 {hh}', f'10{hh}', f'J {hh}', f'Q {hh}', f'K {hh}',
-                        f'A {ss}', f'2 {ss}', f'3 {ss}', f'4 {ss}', f'5 {ss}', f'6 {ss}', f'7 {ss}', f'8 {ss}', f'9 {ss}', f'10{ss}', f'J {ss}', f'Q {ss}', f'K {ss}']
+                    f'A {cc}', f'2 {cc}', f'3 {cc}', f'4 {cc}', f'5 {cc}', f'6 {cc}', f'7 {cc}', f'8 {cc}', f'9 {cc}', f'10{cc}', f'J {cc}', f'Q {cc}', f'K {cc}',
+                    f'A {hh}', f'2 {hh}', f'3 {hh}', f'4 {hh}', f'5 {hh}', f'6 {hh}', f'7 {hh}', f'8 {hh}', f'9 {hh}', f'10{hh}', f'J {hh}', f'Q {hh}', f'K {hh}',
+                    f'A {ss}', f'2 {ss}', f'3 {ss}', f'4 {ss}', f'5 {ss}', f'6 {ss}', f'7 {ss}', f'8 {ss}', f'9 {ss}', f'10{ss}', f'J {ss}', f'Q {ss}', f'K {ss}']
 
 # Function to shuffle the deck
-def shuffle_deck(deck):
+def shuffle(deck):
     shuffled_deck = []
     for i in range (0,52):
         draw_card = random.choice(deck)
@@ -21,26 +20,36 @@ def shuffle_deck(deck):
         shuffled_deck.append(draw_card)
     return shuffled_deck
 
-# Function to print game summary
-def prnt_summ(shuffled_deck, hits):
-    print(f'Dealer showing: {shuffled_deck[len(hits)]}\n')
+# Function to print cards on the table
+def summarize(deck, players, wagers, hits, final=False):
     print(f'-------------------------------------------')
-    for p in range(0,len(hits)):
-        print(f'Player {p+1}: {shuffled_deck[p]}, {shuffled_deck[p+len(hits)+1]}', end='')
-        for c in hits[p]:
-            print(f', {shuffled_deck[c]}', end='')
-        print(f'\n')
+    if final:
+        print(f'Dealer showing:       {deck[players]}, {deck[players+players+1]}')
+        ###################################################################### NEED TO SHOW DEALERS FORCED HIT CARDS
+    else:
+        print(f'Dealer showing: {deck[players]}')
+    print(f'-------------------------------------------')
+    for player in range(players):
+        print(f'Player {player+1}: (${wagers[player]:.0f}) --- {deck[player]}, {deck[player+players+1]}', end='')
+        for hit in hits[player]:
+            print(f', {deck[hit]}', end='')
+        print(f'\n-------------------------------------------')
 
+# Check how many players there are
+players = int(input('How many players are there?\n'))
+hits = [[] for player in range(players)]
+wagers = [0 for player in range(players)]
 
-# Start fresh deck + hit counters
-new_deck = shuffle_deck(unshuffled_deck)
-hits = [[]]
+# Start fresh deck, player, and hit counters
+new_deck = shuffle(unshuffled_deck)
 
-# # EVENTUALLY -- add code to allow multiple players
-# player_count = int(input('How many players are there?\n'))
+# Place bets
+for player in range(players):
+    wagers[player] = float(input(f'How much is Player {player+1} betting?\n'))
+
 
 # Start game
-prnt_summ(new_deck, hits)
+summarize(new_deck, players, wagers, hits, final=False)
 
 
-
+new_deck
