eb10d04999ef2a0f9a55e1fc73f9e58fc212ad8f
Justin K <justinhkiang@gmail.com>
2022-03-22 22:26:24 -0700

    switch to random.shuffle rather than writing my own function. generate deck with list comprehension instead of typing out cards. start function to rotate through player turns and collect wagers

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -1,55 +1,79 @@
 # Dependencies
 import random
 
-# Card Variables
-ss = '\u2660'
-cc = '\u2663'
-hh = '\u2665'
-dd = '\u2666'
-unshuffled_deck = [f'A {dd}', f'2 {dd}', f'3 {dd}', f'4 {dd}', f'5 {dd}', f'6 {dd}', f'7 {dd}', f'8 {dd}', f'9 {dd}', f'10{dd}', f'J {dd}', f'Q {dd}', f'K {dd}',
-                    f'A {cc}', f'2 {cc}', f'3 {cc}', f'4 {cc}', f'5 {cc}', f'6 {cc}', f'7 {cc}', f'8 {cc}', f'9 {cc}', f'10{cc}', f'J {cc}', f'Q {cc}', f'K {cc}',
-                    f'A {hh}', f'2 {hh}', f'3 {hh}', f'4 {hh}', f'5 {hh}', f'6 {hh}', f'7 {hh}', f'8 {hh}', f'9 {hh}', f'10{hh}', f'J {hh}', f'Q {hh}', f'K {hh}',
-                    f'A {ss}', f'2 {ss}', f'3 {ss}', f'4 {ss}', f'5 {ss}', f'6 {ss}', f'7 {ss}', f'8 {ss}', f'9 {ss}', f'10{ss}', f'J {ss}', f'Q {ss}', f'K {ss}']
-
-# Function to shuffle the deck
-def shuffle(deck):
-    shuffled_deck = []
-    for i in range (0,52):
-        draw_card = random.choice(deck)
-        unshuffled_deck.pop(unshuffled_deck.index(draw_card))
-        shuffled_deck.append(draw_card)
-    return shuffled_deck
+# Generate New Deck of Cards
+cards = ['A ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','10','J ','K ']
+suits = ['\u2660','\u2663','\u2665','\u2666']
+deck = [card+suit for suit in suits for card in cards]
 
 # Function to print cards on the table
-def summarize(deck, players, wagers, hits, final=False):
+def summarize(deck, players, money, wagers, hits, final=False):
+    # DEALER's CARDS
     print(f'-------------------------------------------')
     if final:
-        print(f'Dealer showing:       {deck[players]}, {deck[players+players+1]}')
-        ###################################################################### NEED TO SHOW DEALERS FORCED HIT CARDS
+        print(f' Dealer showing:       {deck[players]}, {deck[players+players+1]}', end = '')
+        if hits[-1] > 0:
+            for hit in hits[-1]:
+                print(f', {deck[hit]}', end='')
     else:
-        print(f'Dealer showing: {deck[players]}')
-    print(f'-------------------------------------------')
+        print(f' Dealer showing:       {deck[players]}', end = '')
+    print(f'\n-------------------------------------------')
+    # PLAYER CARDS
     for player in range(players):
-        print(f'Player {player+1}: (${wagers[player]:.0f}) --- {deck[player]}, {deck[player+players+1]}', end='')
+        print(f'Player {player+1} -- ${money[player]:.0f} on the Table --\n')
+        print(f'    Current Bet:  ${wagers[player]:.0f}')
+        print(f'   Current hand:       {deck[player]}, {deck[player+players+1]}', end='')
         for hit in hits[player]:
             print(f', {deck[hit]}', end='')
         print(f'\n-------------------------------------------')
 
-# Check how many players there are
-players = int(input('How many players are there?\n'))
-hits = [[] for player in range(players)]
-wagers = [0 for player in range(players)]
+# Function to give each player a turn
+def play(deck, dealt, hits, players):
+    hit = ''
+    for player in range(players):
+        while hit not in ['stay', 's']:
+            hit = input(f"Player {player+1} - Would you like to hit ('h') or stay ('s')?\n").lower()
+            if hit in ['hit','h']:
+                dealt += 1
+                hits[player].append(dealt)
+                print(f'   Current hand:       {deck[player]}, {deck[player+players+1]}', end='')
+                for hit in hits[player]:
+                    print(f', {deck[hit]}', end='')
+        print('\n-------------------------------------------')
+    return hits
+
+# INTRO
+print('\n\n#############################################')
+print('#####  Welcome to the Blackjack table!  #####')
+print('#############################################')
+
+# Check how many players there are -- eventually maybe move this into the while loop so players can join/leave more easily ?
+players = 0
+while players not in range(1,8):
+    players = int(input('\nHow many players are with you?\nNote: Table seats up to 7\n'))
+    # Reset money, wagers and hits lists based on player count
+    money = [float(input(f'How much does Player {player+1} buy in for?\n')) for player in range(players)]
+    wagers = [0 for player in range(players)]
+    hits = [[] for player in range(players+1)] # Includes DEALER's hits as hits[-1]
 
 # Start fresh deck, player, and hit counters
-new_deck = shuffle(unshuffled_deck)
+playing = True
+while playing:
+    # Shuffle the deck
+    random.shuffle(deck)
 
-# Place bets
-for player in range(players):
-    wagers[player] = float(input(f'How much is Player {player+1} betting?\n'))
+    # Place bets
+    print('\nPlace your bets!')
+    for player in range(players):
+        wagers[player] = float(input(f'How much is Player {player+1} betting?\n'))
 
+    # Deal cards + print summary
+    dealt = (int(players)+1)*2-1
+    summarize(deck, players, money, wagers, hits, final=False)
 
-# Start game
-summarize(new_deck, players, wagers, hits, final=False)
+    ################ need to modify to add insurance option if dealer showing an Ace
 
+    # Loop to give each player a turn
+    for player in range(players):
+        hits = play(deck, dealt, hits, players)
 
-new_deck
