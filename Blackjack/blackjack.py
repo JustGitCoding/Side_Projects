d63620f4eac1a35f75ed03896d6f6906dc05ad8f
Justin K <justinhkiang@gmail.com>
2022-03-25 13:00:47 -0700

    track play through list of cards rather than list of hits. function added for collecting bets, dealing initial set of cards and dealing hit cards. updated summarize function to work with hands instead of hits list

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -6,44 +6,57 @@ cards = ['A ','2 ','3 ','4 ','5 ','6 ','7 ','8 ','9 ','10','J ','K ']
 suits = ['\u2660','\u2663','\u2665','\u2666']
 deck = [card+suit for suit in suits for card in cards]
 
+
+# Function to collect bets
+def collect_wagers(players, money, wagers):
+    for player in range(players):
+        while wagers[player] < 0:
+            wagers[player] = float(input(f'Player {player+1} has ${money[player]} - How much would you like to bet? (minimum = $5)\n'))
+            while wagers[player] > 0 and wagers[player] < 5:
+                wagers[player] = float(input(f"The minimum bet is $5. You can also bet $0 if you do not wish to play this hand.\n"))
+            while wagers[player] > money[player]:
+                wagers[player] = -1
+                wagers[player] = float(input(f'You only have ${money[player]}. How much would you like to bet?\n'))
+    return wagers
+
+# Function to deal initial set of cards
+def deal(deck, players):
+    hands = [[] for player in range(players+1)] # Includes DEALER's hand as hands[-1]
+    for player in range(players+1):
+        hands[player] = [deck[player],deck[player+players]]
+    return hands
+
 # Function to print cards on the table
-def summarize(deck, players, money, wagers, hits, final=False):
+def summarize(deck, players, money, wagers, hands, final=False):
     # DEALER's CARDS
     print(f'-------------------------------------------')
     if final:
-        print(f' Dealer showing:       {deck[players]}, {deck[players+players+1]}', end = '')
-        if hits[-1] > 0:
-            for hit in hits[-1]:
-                print(f', {deck[hit]}', end='')
+        print(f'   Dealer showing:       {hands[-1]}')
     else:
-        print(f' Dealer showing:       {deck[players]}', end = '')
+        print(f'   Dealer showing:       {hands[-1][0]}')
     print(f'\n-------------------------------------------')
     # PLAYER CARDS
     for player in range(players):
-        print(f'Player {player+1} -- ${money[player]:.0f} on the Table --\n')
-        print(f'    Current Bet:  ${wagers[player]:.0f}')
-        print(f'   Current hand:       {deck[player]}, {deck[player+players+1]}', end='')
-        for hit in hits[player]:
-            print(f', {deck[hit]}', end='')
+        print(f'Player {player+1} - ${money[player]:.0f} on the Table\n')
+        print(f'      Current Bet:  ${wagers[player]:.0f}')
+        print(f'     Current hand:       {hands[player]} --- Total: {calc_hand(hands[player])}')
         print(f'\n-------------------------------------------')
 
 # Function to give each player a turn
-def player_turns(deck, dealt, hits, players):
+def player_turns(deck, hands, players):
+    hit = [[] for player in range(players)]
+    player_hit_counter = 0
     for player in range(players):
-        hit = ['ask' for player in range(players)]
-        while hit[player] in ['ask', 'hit', 'h']:
-            hit[player] = input(f"Player {player+1} - Would you like to hit ('h') or stay ('s')?\n").lower()
-            if hit[] in ['hit','h']:
-                dealt += 1
-                hits[player].append(dealt)
-                print(f'   Current hand:       {deck[player]}, {deck[player+players+1]}', end='')
-                #------- add something here
-                # -------------- print total for the player
-                for hit in hits[player]:
-                    print(f', {deck[hit]}', end='')
-                print('\n')
+        while hit[player] not in ['stay', 's']:
+            hand_total = calc_hand(hands[player])
+            print(f"Player {player+1} -")
+            print(f"     Current hand:       {hands[player]} --- Total: {hand_total} ----- (Dealer showing: {hands[-1][0]})")
+            hit[player] = input(f"Would you like to hit ('h') or stay ('s')?\n").lower()
+            if hit[player] in ['hit','h']:
+                player_hit_counter += 1
+                hands[player].append(deck[(players*2)+player_hit_counter])
         print('\n-------------------------------------------')
-    return hits
+    return hands
 
 # Function to calculate a hand value
 def calc_hand(hand):
@@ -69,15 +82,14 @@ print('#############################################')
 
 # Check how many players there are -- eventually maybe move this into the while loop so players can join/leave more easily ?
 players = 0
-while players not in range(1,8):
+while players == 0:
     players = int(input('\nHow many players are with you?\nNote: Table seats up to 7\n'))
     # Reset money, wagers and hands lists based on player count
     money = [float(input(f'How much does Player {player+1} buy in for?\n')) for player in range(players)]
-    wagers = [0 for player in range(players)]
-    hits = [[] for player in range(players+1)] # Includes DEALER's hits as hits[-1] ######## DO WE STILL NEED THIS????
-    hands = [[] for player in range(players+1)] # Includes DEALER's hand as hands[-1]
+    wagers = [-1 for player in range(players)]
+    
 
-# Start fresh deck, player, and hit counters
+# Keep playing as long as there is money on the table
 table_money = 0
 for m in money:
     table_money += m
@@ -86,23 +98,17 @@ while table_money > 0:
     random.shuffle(deck)
 
     # Place bets
-    print('\nPlace your bets!')
-    for player in range(players):
-        while money[player] >= wagers[player]:
-            wagers[player] = float(input(f'Player {player+1} has ${money[player]} - How much would you like to bet?\n'))
-            if wagers[player] > money[player]:
-                wagers[player] = 0
-                print(f'You only have ${money[player]}.')
+    print('\n~~~ Place your bets! ~~~')
+    wagers = collect_wagers(players, money, wagers)
 
     # Deal cards + print summary
-    dealt = (int(players)+1)*2-1
-    summarize(deck, players, money, wagers, hits, final=False)
+    hands = deal(deck, players)
+    summarize(deck, players, money, wagers, hands, final=False)
 
     ################ need to modify to add insurance option if dealer showing an Ace
 
-    # Loop to give each player a turn
-    for player in range(players):
-        hits = player_turns(deck, dealt, hits, players)
+    # Loop to give each player a turn:
+    hands = player_turns(deck, hands, players)
 
     # Run Function for Dealer's turn
 
@@ -115,12 +121,3 @@ while table_money > 0:
     for m in money:
         table_money += m
 
-
-
-hits = [[],['hello'],[]]
-
-for hit in hits:
-    if hit:
-        print('good')
-    else:
-        print(hit)
